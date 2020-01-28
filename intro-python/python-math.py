c3b5a058191286e6331821f93d8f42ee4915ec1b
ret2ex <kosongisnotzero@gmail.com>
2020-01-28 19:35:39 +0700

    Introduction Python

---
+++ intro-python/python-math.py
@@ -0,0 +1,10 @@
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+print(100*(1.1**7))
---
+++ intro-python/python-variable.py
@@ -0,0 +1,15 @@
+savings = 100 #int
+growth_multiplier = 1.1 #float
+desc = "compound interest" #string
+
+# Assign product of growth_multiplier and savings to year1
+year1=savings*growth_multiplier
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc=desc+desc
+
+# Print out doubledesc
+print(doubledesc)
